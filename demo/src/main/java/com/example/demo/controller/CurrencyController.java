1641b8fc2669faecd854628cbd2718993d642d7e
Timo Ji <timo.ji@student.htw-berlin.de>
2022-06-26 08:52:00 +0200

    rabbitmq implemented

---
+++ demo/src/main/java/com/example/demo/controller/CurrencyController.java
@@ -0,0 +1,72 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@CrossOrigin
+public class CurrencyController {
+
+    private final String API_KEY = "SGZliVw2kVfH3lXpOjLvJyXzp5b1iqdB";
+
+    @GetMapping("/currency")
+    String getCurrency(){
+        return "Hello Currency";
+    }
+
+    @GetMapping("https://api.apilayer.com/exchangerates_data/latest")
+    String getInfo() {
+        return "hello info";
+    }
+
+    @GetMapping("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}")
+    String getResponse(@RequestHeader(name = "apikey", value = API_KEY) String s, @PathVariable String amount, @PathVariable String from, @PathVariable String to) {
+    return "hello";
+//    HttpClie client = new HttpClient.Builder().build();
+//
+//    Request request = new Request.Builder()
+//            .url("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}")
+//            .addHeader("apikey", "{API-KEY}")
+//            .method("GET", })
+//            .build();
+//        Response response = client.newCall(request).execute();
+//        System.out.println(response.body().string());
+
+    }
+
+    @GetMapping("/countries")
+    String getCountries() {
+        String uri = "https://restcountries.com/v3.1/all\n";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("/exchangeRateLatest")
+    String getExchangeRateLatest() {
+        String uri = "https://api.exchangerate.host/latest";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("/exchangeRateSpecified")
+    String getExchangeRateSpecified() {
+        String uri = "https://api.exchangerate.host/latest?access_key=SGZliVw2kVfH3lXpOjLvJyXzp5b1iqdB&base=USD&symbols=GBP,JPY,USD";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("/exchangeRateResponseBody")
+    @ResponseBody
+    String getExchangeRateResponseBody() {
+        String uri = "https://api.exchangerate.host/latest?access_key=SGZliVw2kVfH3lXpOjLvJyXzp5b1iqdB&base=USD&symbols=GBP,JPY,USD";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        ResponseEntity<String> re = new ResponseEntity<String>(result, HttpStatus.MULTI_STATUS);
+        return re.getHeaders().getFirst("*");
+    }
+}
---
+++ demo/src/main/java/rabbitmq/Receiver.java
@@ -0,0 +1,24 @@
+package rabbitmq;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    /**
+     *  This lets it signal that the message has been received.
+     *  This is something you are not likely to implement in a production application.
+     */
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+}
