5112d4ce0e8c521d1a6789b440c6d3be1b93c08f
Timo Ji <timo.ji@student.htw-berlin.de>
2022-09-27 21:58:38 +0200

    removed alternative exchange rate api, refactor complete

--- src/main/java/com/example/demo/service/CurrencyService.java
+++ src/main/java/com/example/demo/service/CurrencyService.java
@@ -5,20 +5,12 @@ import com.example.demo.model.Currency;
 import org.springframework.web.client.RestTemplate;
 
 public class CurrencyService {
-//    private final String API_KEY = "9VBbjmAQznWLbQYLaG8bzuO6QLG58kVc";
     private final String ACCESS_KEY = "SGZliVw2kVfH3lXpOjLvJyXzp5b1iqdB";
 
     public String getExchangeRateSpecified(String oldCurrency, String newCurrency) throws CurrencyNotFoundException {
         if(!Currency.currencyExist(oldCurrency) || !Currency.currencyExist(newCurrency)) {
             throw new CurrencyNotFoundException();
         }
-        // use this ALTERNATIVE exchange rate api if the server of the other on is down !!!
-//        String uri = "https://api.apilayer.com/exchangerates_data/latest?apikey=" + API_KEY + "&base=" + oldCurrency + "&symbols=" + newCurrency;
-//        RestTemplate restTemplate = new RestTemplate();
-//        String result = restTemplate.getForObject(uri, String.class);
-//        String[] splittedResult = result.split(newCurrency + "\": ");
-//        String[] splittedResult2 = splittedResult[1].split("}");
-//        String rate = splittedResult2[0].substring(0, (splittedResult2[0].length() - 5));
 
         String uri = "https://api.exchangerate.host/latest?access_key=" + ACCESS_KEY + "&base=" + oldCurrency + "&symbols=" + newCurrency;
         RestTemplate restTemplate = new RestTemplate();
